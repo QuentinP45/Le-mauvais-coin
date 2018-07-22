0a82a2eb61b4edf5d7842787b9860a54f0a94216
Quentin Picart <quentin.picart@gmail.com>
2018-07-22 16:05:03 +0200

    feat : create method to list all offers

--- src/Controller/OfferController.php
+++ src/Controller/OfferController.php
@@ -2,18 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Offer;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * Offer controller
+ * @Route("offer")
+ */
 class OfferController extends Controller
 {
     /**
      * @Route("/list-user", name="index_offers_user")
+     * @Method("GET")
      */
     public function index()
     {
-        return $this->render('user/index.html.twig', [
+        $em = $this->getDoctrine()->getManager();
+        $offers = $em->getRepository(Offer::class)->findAll();
 
+        return $this->render('user/index.html.twig', [
+            'offers' => $offers,
         ]);
     }
 }
