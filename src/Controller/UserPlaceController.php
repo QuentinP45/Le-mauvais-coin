a65ad8f627b43abb26f7d34ebd0e2c95850e5764
Quentin Picart <quentin.picart@gmail.com>
2018-07-24 09:53:58 +0200

    feat : change view userPlace the user can see his own offers and create new ones in the same page

--- src/Controller/UserPlaceController.php
+++ src/Controller/UserPlaceController.php
@@ -9,6 +9,8 @@
 namespace App\Controller;
 
 use App\Entity\Offer;
+use App\Form\OfferType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -17,14 +19,33 @@ class UserPlaceController extends AbstractController
     /**
      * @Route("/user-place", name="user_place")
      */
-    public function userPlace()
+    public function userPlace(Request $request)
     {
         $user = $this->getUser();
         $userOffers = $this->getDoctrine()->getManager()
             ->getRepository(Offer::class)->findUserOffers($user);
 
+        $offer = new Offer();
+        $user = $this->getUser();
+        $offer->setCreator($user);
+        $form = $this->createForm(OfferType::class, $offer);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($offer);
+            $em->flush();
+
+            return $this->redirectToRoute('user_place', [
+                'userOffers' => $userOffers,
+                'form' => $form->createView(),
+            ]);
+        }
+
         return $this->render('user/userPlace.html.twig', [
-            'userOffers' => $userOffers
+            'userOffers' => $userOffers,
+            'form' => $form->createView(),
         ]);
     }
 }
